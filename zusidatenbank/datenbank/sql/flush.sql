a2f2688e187a835e134b5a5e35e4a7cd0e8ac542
Jonathan Pilborough <jonathan@pilborough.co.uk>
2017-10-14 09:46:57 +0200

    Flush all sequences

--- zusidatenbank/datenbank/sql/flush.sql
+++ zusidatenbank/datenbank/sql/flush.sql
@@ -1,5 +1,16 @@
 BEGIN;
 TRUNCATE "datenbank_streckenmodule_nachbaren", "datenbank_fuehrerstand", "datenbank_fuehrerstand_autor", "datenbank_streckenmodule", "datenbank_fahrplanzug_autor", "datenbank_fahrplanzug", "datenbank_fahrplan", "datenbank_fahrplan_zuege", "datenbank_autor", "datenbank_fahrzeugvariante_autor", "datenbank_fahrplanzugeintrag", "datenbank_fahrplan_strecken_modules", "datenbank_fahrplanzug_fahrzeuge", "datenbank_fahrzeugvariante", "datenbank_fahrplan_autor", "datenbank_streckenmodule_autor";
-SELECT setval(pg_get_serial_sequence('"datenbank_fahrzeugvariante"','id'), 1, false);
+
 SELECT setval(pg_get_serial_sequence('"datenbank_fahrplanzugeintrag"','id'), 1, false);
+SELECT setval(pg_get_serial_sequence('"datenbank_streckenmodule_nachbaren"','id'), coalesce(max("id"), 1), max("id") IS NOT null) FROM "datenbank_streckenmodule_nachbaren";
+SELECT setval(pg_get_serial_sequence('"datenbank_streckenmodule_autor"','id'), coalesce(max("id"), 1), max("id") IS NOT null) FROM "datenbank_streckenmodule_autor";
+SELECT setval(pg_get_serial_sequence('"datenbank_fuehrerstand_autor"','id'), coalesce(max("id"), 1), max("id") IS NOT null) FROM "datenbank_fuehrerstand_autor";
+SELECT setval(pg_get_serial_sequence('"datenbank_fahrzeugvariante_autor"','id'), coalesce(max("id"), 1), max("id") IS NOT null) FROM "datenbank_fahrzeugvariante_autor";
+SELECT setval(pg_get_serial_sequence('"datenbank_fahrzeugvariante"','id'), coalesce(max("id"), 1), max("id") IS NOT null) FROM "datenbank_fahrzeugvariante";
+SELECT setval(pg_get_serial_sequence('"datenbank_fahrplanzug_fahrzeuge"','id'), coalesce(max("id"), 1), max("id") IS NOT null) FROM "datenbank_fahrplanzug_fahrzeuge";
+SELECT setval(pg_get_serial_sequence('"datenbank_fahrplanzug_autor"','id'), coalesce(max("id"), 1), max("id") IS NOT null) FROM "datenbank_fahrplanzug_autor";
+SELECT setval(pg_get_serial_sequence('"datenbank_fahrplanzugeintrag"','id'), coalesce(max("id"), 1), max("id") IS NOT null) FROM "datenbank_fahrplanzugeintrag";
+SELECT setval(pg_get_serial_sequence('"datenbank_fahrplan_strecken_modules"','id'), coalesce(max("id"), 1), max("id") IS NOT null) FROM "datenbank_fahrplan_strecken_modules";
+SELECT setval(pg_get_serial_sequence('"datenbank_fahrplan_zuege"','id'), coalesce(max("id"), 1), max("id") IS NOT null) FROM "datenbank_fahrplan_zuege";
+SELECT setval(pg_get_serial_sequence('"datenbank_fahrplan_autor"','id'), coalesce(max("id"), 1), max("id") IS NOT null) FROM "datenbank_fahrplan_autor";
 COMMIT;
