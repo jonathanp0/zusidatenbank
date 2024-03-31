80cba2c5604ac7889825b55c983993cfc5c226c3
jonathanp0 <jonathan@pilborough.co.uk>
2024-04-01 01:54:56 +0200

    Hide Start link unless enabled

--- zusidatenbank/datenbank/templatetags/zusistart_button.py
+++ zusidatenbank/datenbank/templatetags/zusistart_button.py
@@ -3,9 +3,8 @@ from django import template
 
 register = template.Library()
 
-
 @register.inclusion_tag("include/zusistart_button.html")
-def zusistart_button(fpn_path, zug_nummer, mini):
+def zusistart_button(show, fpn_path, zug_nummer, mini):
     encodedpath = fpn_path.replace("\\", "?")
 
-    return {"fpn": encodedpath, "nummer": zug_nummer, "mini": mini}
+    return {"show": show, "fpn": encodedpath, "nummer": zug_nummer, "mini": mini}
